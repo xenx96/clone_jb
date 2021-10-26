fe61f18d5d7b74b9c0f9ee993ba2e31473e529d1
DaeSun Yoo <xenx96@naver.com>
2021-10-26 12:55:14 +0900

    RealEstateAgent Find
    With Swagger REST API.
    getting RealEstateAgent Info from goverment Open API.

--- NodeJS/swagger_test/swagger.js
+++ NodeJS/swagger_test/swagger.js
@@ -1,29 +1,30 @@
-import swaggerUi from 'swagger-ui-express';
-import swaggereJsdoc from 'swagger-jsdoc';
+import swaggerUi from "swagger-ui-express";
+import swaggereJsdoc from "swagger-jsdoc";
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: "Clone_JB's Node Express API with Swagger",
-            version: '0.1.0',
-            description: 'This is a simple CRUD API application made with Express and documented with Swagger',
-            license: {
-                name: 'MIT',
-                url: 'https://spdx.org/licenses/MIT.html',
-            },
-            contact: {
-                name: 'Meditator',
-                url: 'https://github.com/xenx96/clone_jb',
-                email: 'xenx96@naver.com',
-            },
-        },
-        servers: [
-            {
-                url: 'http://localhost:8080/',
-            },
-        ],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Clone_JB's Node Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Meditator",
+        url: "https://github.com/xenx96/clone_jb",
+        email: "xenx96@naver.com",
+      },
     },
-    apis: ['./swagger_test/service/*'],
+    servers: [
+      {
+        url: "http://localhost:8080/",
+      },
+    ],
+  },
+  apis: ["./swagger_test/service/*"],
 };
 export const specs = swaggereJsdoc(options);
 export const swaggerUI = swaggerUi;
