3a89ab10f4c171e5ab289a9be88acc77d808b830
xenx96 <75344540+xenx96@users.noreply.github.com>
2021-10-28 22:22:18 +0900

    JWT관련 인증 Swagger Test
    현재 Cookie-Parser 작동이 안되고있음.
    Swagger 버전에 따른 문제로
    https://swagger.io/docs/specification/authentication/cookie-authentication/
    참고하여 추후 보완할 예정.

--- NodeJS/query/companyQuery.js
+++ NodeJS/query/companyQuery.js
@@ -1,39 +1,39 @@
-import * as company from '../schemas/company.js';
+import * as company from "../schemas/company.js";
 const CP = company.model;
 
-export const findByID = id => {
-    return CP.findOne({ ID: id });
+export const findByID = (id) => {
+  return CP.findOne({ ID: id });
 };
 
-export const findByCID = CID => {
-    return CP.findOne({ CID });
+export const findByCID = (CID) => {
+  return CP.findOne({ CID });
 };
 //Form is JSON or Map.
 export const updateByID = (id, updateForm) => {
-    const UA = new Date.now();
-    updateForm.UA = UA;
-    try {
-        return CP.findOneAndUpdate({ ID: id }, { updateForm }, { new: true });
-    } catch (e) {
-        console.error(e);
-        return e;
-    }
+  const UA = new Date.now();
+  updateForm.UA = UA;
+  try {
+    return CP.findOneAndUpdate({ ID: id }, { updateForm }, { new: true });
+  } catch (e) {
+    console.error(e);
+    return e;
+  }
 };
 
-export const deleteByID = id => {
-    try {
-        return CP.findOneAndDelete({ ID: id });
-    } catch (e) {
-        console.error(e);
-        return e;
-    }
+export const deleteByID = (id) => {
+  try {
+    return CP.findOneAndDelete({ ID: id });
+  } catch (e) {
+    console.error(e);
+    return e;
+  }
 };
 //
-export const Insert = insertForm => {
-    try {
-        return CP.create(insertForm);
-    } catch (e) {
-        console.error(e);
-        return e;
-    }
+export const Insert = (insertForm) => {
+  try {
+    return CP.create(insertForm);
+  } catch (e) {
+    console.error(e);
+    return e;
+  }
 };
--- NodeJS/service/companyService.js
+++ NodeJS/service/companyService.js
@@ -1,32 +1,32 @@
 //import Express from 'express';
 //const router = Express().router;
-import * as hash from '../middleware/bcryptHash.js';
-import * as CQ from '../query/companyQuery.js';
+import * as hash from "../middleware/bcryptHash.js";
+import * as CQ from "../query/companyQuery.js";
 
 export const signInUser = async (req, res) => {
-    const companyForm = req.body;
-    try {
-        var hashedPW = await hash.encodeHash(companyForm.PW);
-        companyForm.PW = hashedPW;
-        res.send(CQ.Insert(companyForm));
-    } catch (e) {
-        console.error(e);
-        res.send(false);
-    }
+  const companyForm = req.body;
+  try {
+    var hashedPW = await hash.encodeHash(companyForm.PW);
+    companyForm.PW = hashedPW;
+    res.send(CQ.Insert(companyForm));
+  } catch (e) {
+    console.error(e);
+    res.send(false);
+  }
 };
 
-const msg = '사용 가능합니다.';
+const msg = "사용 가능합니다.";
 
-export const idCheck = async id => {
-    if ((await CQ.findByID(id)) == null) {
-        return msg;
-    }
-    throw '이미 사용중인 아이디입니다.';
+export const idCheck = async (id) => {
+  if ((await CQ.findByID(id)) == null) {
+    return msg;
+  }
+  throw "이미 사용중인 아이디입니다.";
 };
 
-export const CIDCheck = async CID => {
-    if ((await CQ.findByCID(CID)) == null) {
-        return msg;
-    }
-    throw '이미 사용중인 부동산 등록번호 입니다.';
+export const CIDCheck = async (CID) => {
+  if ((await CQ.findByCID(CID)) == null) {
+    return msg;
+  }
+  throw "이미 사용중인 부동산 등록번호 입니다.";
 };
